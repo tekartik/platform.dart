534f1985937ccf5e62b6d995455f7bd17935325d
Alexandre Roux <alex@tekartik.com>
2018-11-23 16:44:15 +0100

    fix: warnings

--- platform_browser/test/quick_snippet_test.dart
+++ platform_browser/test/quick_snippet_test.dart
@@ -3,8 +3,6 @@ library platform_context.test.quick_snippet.dart;
 import 'package:dev_test/test.dart';
 import 'package:tekartik_platform_browser/src/browser_detect_common.dart';
 
-import 'user_agents.dart';
-
 //
 // Dart vs Javascript detection
 //
@@ -20,7 +18,6 @@ bool canBeUserAgentChrome(String userAgent) => userAgent.contains('Chrome');
 
 bool isUserAgentIe(String userAgent) {
   // Yoga IE 11: Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; Touch; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; Tablet PC 2.0; MALNJS; rv:11.0) like Gecko
-  // Yoga Edge 12: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240
   return userAgent.contains('Trident');
 }
 
