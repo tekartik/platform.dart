b61fafe7079865d1fa578aebce9c2ec8e07b0f4d
alex <alex@tekartik.com>
2020-11-15 10:30:55 +0100

    feat: publish to none

--- platform_browser/tool/travis.dart
+++ platform_browser/tool/travis.dart
@@ -1,11 +1,12 @@
+import 'package:dev_test/package.dart';
 import 'package:process_run/shell.dart';
 
 Future main() async {
   var shell = Shell();
+  await ioPackageRunCi('.');
 
+  // added chrome test
   await shell.run('''
-dartanalyzer --fatal-warnings --fatal-infos .
-pub run build_runner test -- -p vm,chrome
-pub run test -p vm,chrome
+pub run build_runner test -- -p chrome
 ''');
 }
