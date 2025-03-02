21a479d9bf4f989dc8ab682646f3c77019ca3ca0
Alexandre Roux <alex@tekartik.com>
2025-03-02 11:08:22 +0100

    fix export for platform

--- platform_io/lib/src/export/export.dart
+++ platform_io/lib/src/export/export.dart
@@ -1,16 +1,16 @@
 import 'package:tekartik_platform/context.dart';
-import 'package:tekartik_platform_io/context_io.dart';
 
+import '../../context_io.dart';
 import 'export_stub.dart' if (dart.library.io) 'export_io.dart' as impl;
 
 /// Io platform
 Platform get platformIo => platformContextIo.platform!;
 
-/// Io platform
-Platform? get platformIoOrNull => platformContextIo.platform;
+/// Io platform, null on other platforms.
+Platform? get platformIoOrNull => platformContextIoOrNull?.platform;
 
 /// IO platform content, null on other platforms.
-PlatformContextIo get platformContextIo => impl.platformContextIoOrNull!;
+PlatformContextIo get platformContextIo => impl.platformContextIo;
 
 /// IO platform content, null on other platforms.
 PlatformContextIo? get platformContextIoOrNull => impl.platformContextIoOrNull;
--- platform_io/lib/src/export/export_io.dart
+++ platform_io/lib/src/export/export_io.dart
@@ -1,6 +1,10 @@
 import 'package:tekartik_platform_io/context_io.dart';
 import 'package:tekartik_platform_io/src/context_io.dart' as context_io;
 
+/// IO platform content, throws on other platforms.
+PlatformContextIo get platformContextIo =>
+    context_io.platformContextIoSingleton;
+
 /// IO platform content.
 PlatformContextIo? get platformContextIoOrNull =>
     context_io.platformContextIoSingleton;
